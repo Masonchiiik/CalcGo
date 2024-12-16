8e67d07ba5835f78686ec6b69494dae670356350
Mason <arsenijstrelcov577@gmail.ru>
2024-12-16 17:17:33 +0300

    ErrorFix

--- internal/application/httpError.go
+++ internal/application/httpError.go
@@ -1,7 +1,6 @@
 package application
 
 var (
-	errorInternal       = `{"error": "Internal server error"}`
-	errorExpression     = `{"error": "Expression is not valid"}`
-	errorDivisionByZero = `{"error" : "Division by zero"}`
+	errorInternal   = `{"error": "Internal server error"}`
+	errorExpression = `{"error": "Expression is not valid"}`
 )
