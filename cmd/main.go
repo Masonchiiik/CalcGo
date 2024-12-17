e87baf5ac83b0a17f2933f705d93b64b3f4329c2
Mason <arsenijstrelcov577@gmail.ru>
2024-12-17 12:07:09 +0300

    Добавлено приветствие

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/Masonchiiik/CalcGo/internal/application"
+import (
+	"fmt"
+
+	"github.com/Masonchiiik/CalcGo/internal/application"
+)
 
 func main() {
+	fmt.Printf("%v", "Привет лицеист, хорошего тебе дня), и удачи в пользовании программы(если что-то непонятно почитай README, он сделан специально для тебя)")
 	app := application.New()
 	app.StartServer()
 }
