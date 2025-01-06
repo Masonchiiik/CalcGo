0695e9fdf7cc13059b1d42e5ee5de42ecec1d365
Mason <arsenijstrelcov577@gmail.ru>
2025-01-06 20:58:32 +0300

    update application_test.go

--- internal/application/httpError.go
+++ internal/application/httpError.go
@@ -1,6 +1,6 @@
 package application
 
-var (
+const (
 	errorInternal   = `{"error": "Internal server error"}`
 	errorExpression = `{"error": "Expression is not valid"}`
 	errorMethod     = `{"error": "Only POST method available"}`
